788e0b6939f8b7ae305ad7d4f22e642c7bb31a10
Colin Busby <cbusby@isl.ca>
2016-08-29 16:13:04 -0300

    Updating module to support latest Instagram changes.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,8 @@ Drupal.behaviors.drupal_instagram_feed = {
     attach: function () {
         var feed = new Instafeed({
             get: 'user',
-            userId: parseInt(Drupal.settings.drupal_instagram_feed.user_id),
+            userId: Drupal.settings.drupal_instagram_feed.user_id,
+            clientId: Drupal.settings.drupal_instagram_feed.client_id,
             accessToken: Drupal.settings.drupal_instagram_feed.access_token,
             resolution: Drupal.settings.drupal_instagram_feed.image_size,
             limit: Drupal.settings.drupal_instagram_feed.num_images,
